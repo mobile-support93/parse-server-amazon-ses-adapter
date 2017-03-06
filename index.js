2dadb47327a8f3e24b845e6bd0fc4974d78f4c97
mobile-support93 <mobile-support93@users.noreply.github.com>
2017-03-06 16:22:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,79 @@
+
 var AmazonSES = require("amazon-ses-mailer");
 
-module.exports=function(options){
+module.exports = function (options) {
+    var bannerImage=options.bannerImage;
+    var ses = new AmazonSES(
+        options.accessKeyId,
+        options.secretAccessKey,
+        options.region
+    );
 
-   var ses = new AmazonSES(
-      options.accessKeyId,
-      options.secretAccessKey,
-      options.region
-   );
+    var sendMail = function (mail) {
 
-   var sendMail=function(mail){
+        return new Promise(function (resolve, reject) {
 
-      return new Promise(function(resolve, reject){
+            ses.send({
+                from: options.from,
+                to: [mail.to],
+                subject: mail.subject,
+                body: {
+                    text: mail.text
+                }
+            }, function (error, data) {
 
-         ses.send({
-            from: options.from,
-            to: [mail.to],
-            subject: mail.subject,
-            body: {
-               text: mail.text
-            }
-         }, function(error, data) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    };
 
-            if (error) {
-               reject(error);
-            } else {
-               resolve(data);
-            }
-         });
-      });
-   };
+    var sendVerificationEmail = function (params) {                        
+        return new Promise(function (resolve, reject) {
+         var mailBody=   '<span style="text-align: center;"><img src="'+bannerImage+'" alt="' +params.appName+' Logo" style="height: 200px"></span>'+
+'<br />'+
+'<p>Hi,</p>'+
 
-   return {
-      sendMail: sendMail
-   }
+'<p>Thank you for creating an account! <br />'+
+
+'Please click <a href="'+params.link+'">HERE</a> to confirm the use of this email address with '+params.appName+'.</p>'+
+
+'Thanks,<br />'+
+
+params.appName+' Support';
+
+            ses.send({
+                from: options.from,
+                to: [params.to],
+                subject: "",
+                body: {
+                    html: mailBody,
+                }
+            }, function (error, data) {
+
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+
+
+    }
+    var sendPasswordResetEmail = function (options) {
+
+    }
+
+    return {
+        sendMail: sendMail,
+        sendVerificationEmail: sendVerificationEmail,
+        sendPasswordResetEmail: sendPasswordResetEmail
+    }
 };
+
+
+
